6fb63c6f3512306e7ceef94cf56bc43b3c7ecec9
lethean-kun <1434745143@qq.com>
2023-05-16 22:15:49 +0800

    pwd init

--- init.sql
+++ init.sql
@@ -1,11 +1,12 @@
-CREATE DATABASE sunmote;
+CREATE
+DATABASE sunmote;
 
 create table sunmote.User
 (
     id        bigint unsigned auto_increment primary key,
     username  varchar(64)                           not null comment '用户名',
     password  varchar(255)                          not null comment '密码',
-    corpName  varchar(64)                           null,
+    corpName  varchar(64) null,
     status    tinyint     default 0                 not null comment '状态',
     rebate    double      default 0.12              not null comment '返点',
     isDelete  tinyint     default 0                 not null comment '是否已删除',
@@ -14,13 +15,16 @@ create table sunmote.User
     role      varchar(64) default 'USER'            not null comment '角色'
 ) comment '用户表';
 
+insert into sunmote.User (username, password, corpName, status, rebate, isDelete, role)
+    value ('admin','$2a$10$m4Zpfj/pWDfqV4sBqs6f0O9mTGwOnVhkIDk90u2vieHjxGzxXNZii','光晨科技',0,0.12,0,'ADMIN');
+
 create table sunmote.DepositApproval
 (
     id        bigint unsigned auto_increment
         primary key,
-    deposit   varchar(64)                           null comment '付款金额',
-    userId    bigint unsigned                       not null comment '用户ID',
-    remark    varchar(255)                          null comment '邮箱',
+    deposit   varchar(64) null comment '付款金额',
+    userId    bigint unsigned not null comment '用户ID',
+    remark    varchar(255) null comment '邮箱',
     currency  varchar(16) default 'USD'             not null,
     status    tinyint     default 0                 not null comment '状态',
     isDelete  tinyint     default 0                 not null,
@@ -35,17 +39,16 @@ create table sunmote.CreateAccountApproval
     id             bigint unsigned auto_increment
         primary key,
     timeZone       varchar(64)                           not null comment '时区',
-    accountId      varchar(32)                           null comment '账户ID',
+    accountId      varchar(32) null comment '账户ID',
     currency       varchar(16) default 'USD'             not null comment '币种',
-    rechargeAmount double      default 0                 null comment '首次充值金额',
-    prodLink       varchar(128)                          null comment '产品链接',
-    userId         bigint unsigned                       not null comment '用户ID',
+    rechargeAmount double      default 0 null comment '首次充值金额',
+    prodLink       varchar(128) null comment '产品链接',
+    userId         bigint unsigned not null comment '用户ID',
     status         tinyint     default 0                 not null comment '状态',
     isDelete       tinyint     default 0                 not null comment '是否已删除',
     createdAt      datetime    default CURRENT_TIMESTAMP not null comment '创建时间',
     updatedAt      datetime    default CURRENT_TIMESTAMP not null on update CURRENT_TIMESTAMP comment '更新时间'
-)
-comment '开户申请';
+) comment '开户申请';
 
 
 
@@ -57,13 +60,12 @@ create table sunmote.AccountRechargeApproval
     rechargeAmount       double                             not null comment '充值金额',
     payAmount            double                             not null comment '实际支付金额',
     targetCurrency       varchar(16)                        not null comment '目标币种',
-    exchangeRate         double                             null comment '汇率',
-    targetCurrencyAmount double                             null comment '汇率结算金额',
-    userId               bigint unsigned                    not null comment '用户ID',
+    exchangeRate         double null comment '汇率',
+    targetCurrencyAmount double null comment '汇率结算金额',
+    userId               bigint unsigned not null comment '用户ID',
     status               tinyint  default 0                 not null comment '状态',
     isDelete             tinyint  default 0                 not null comment '是否已删除',
     createdAt            datetime default CURRENT_TIMESTAMP not null comment '创建时间',
     updatedAt            datetime default CURRENT_TIMESTAMP not null on update CURRENT_TIMESTAMP comment '更新时间'
-)
-comment '账户充值申请';
+) comment '账户充值申请';
 
