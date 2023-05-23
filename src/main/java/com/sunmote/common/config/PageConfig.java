ae0f9435f37487df7f24731caf497a2fea4127cf
lethean-kun <1434745143@qq.com>
2023-05-23 23:37:47 +0800

    分页bug修复

---
+++ src/main/java/com/sunmote/common/config/PageConfig.java
@@ -0,0 +1,14 @@
+package com.sunmote.common.config;
+
+import com.baomidou.mybatisplus.extension.plugins.PaginationInterceptor;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class PageConfig {
+
+    @Bean
+    public PaginationInterceptor paginationInterceptor() {
+        return new PaginationInterceptor();
+    }
+}
