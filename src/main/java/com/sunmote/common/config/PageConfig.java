47ea739db57871c99ff8331f7e76640dff8d52c4
lethean-kun <1434745143@qq.com>
2023-11-21 19:04:12 +0800

    1、展示两位小数 2、修复分页bug

--- src/main/java/com/sunmote/common/config/PageConfig.java
+++ src/main/java/com/sunmote/common/config/PageConfig.java
@@ -1,11 +1,24 @@
 package com.sunmote.common.config;
 
 import com.baomidou.mybatisplus.extension.plugins.PaginationInterceptor;
+import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
 public class PageConfig {
+
+    final ApplicationContext applicationContext;
+
+    public PageConfig(ApplicationContext applicationContext) {
+        this.applicationContext = applicationContext;
+    }
+
+    @Bean
+    public void paginationInterceptorPageLimit() {
+        PaginationInterceptor bean = applicationContext.getBean(PaginationInterceptor.class);
+        bean.setLimit(-1);
+    }
 
     @Bean
     public PaginationInterceptor paginationInterceptor() {
